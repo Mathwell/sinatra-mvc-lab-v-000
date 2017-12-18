3357c82eecc7c9c43e3739335acbacb4ab928ab9
Olena Ageyeva <eageeva@hotmail.com>
2017-12-18 01:07:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,4 +5,10 @@ class App < Sinatra::Base
   get '/' do
     erb :user_input
   end
+
+  post '/' do
+  text_from_user = params[:user_text]
+  @text_piglatinized=PigLatinizer.new(text_from_user)
+  erb :results
+end
 end
