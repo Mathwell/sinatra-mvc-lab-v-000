09c435cbadbe7904eae38617887f6e95e0429a02
Olena Ageyeva <eageeva@hotmail.com>
2017-12-18 00:52:25 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,7 +4,22 @@ class PigLatinizer
   end
 
   def piglatinize(word)
-    new_word=word[1..-1]+word[0]+"ay"
+   if starts_with_consonant?(word)
+    arr=word.split(/[aeoui]/)
+    cons=arr.shift
+    new_word=arr.to_s+cons+"ay"
+   else
+     new_word=word+"way"
+   end
+
+   def starts_with_consonant?(word)
+     if "bcdfghjklmnpqrstvwxyz".include?(word[0])
+       return true
+     else
+       return false
+     end
+   end
+    
   end
 
 end
